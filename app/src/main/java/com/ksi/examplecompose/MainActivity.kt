97946ce394b8ce1d591ed3b8f9594c0c333f99a3
Mohamed Altohamy <mohamed.altohamy@kickstartinteractive.net>
2022-11-13 13:31:50 +0200

    ezz

--- app/src/main/java/com/ksi/examplecompose/MainActivity.kt
+++ app/src/main/java/com/ksi/examplecompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ksi.examplecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,40 +14,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ksi.examplecompose.ui.theme.ExampleComposeTheme
 import com.ksi.examplecompose.ui.theme.defaultPadding
 import java.util.Collections.list
-/*
-coupling class a and b depend to each other is bad practice
-solid
-s->single responsibility
-class should have one responsibilty and have one reason to change
-benifits
-1- easy testing
-2- lower coupling
-3-easy to understand-clean code
-4- code organized
-5- if function have problem will not effect another if all function in smae class
-o->open close (software entities (classes,module,functions)
-should be open for extention close for modification
-l->liskov substitution
-if s is asubtype of t then objects of type t in aprogram may be replaced with object of type s
-without altering any of the desirsble properites of that programe
 
-i-> Interface segregation(seperate)
-splite large interfce to small interface
-so client know only about methods they need
-d->dependency inversion العكسى
-high level module and low model both should depend on abstration
-abstraction should not depend on details but concret class have details
-
-
-
-
- */
 class MainActivity : ComponentActivity() {
+    val viewModel :MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().also {
+             viewModel.isLoading
+        }
         setContent {
             ExampleComposeTheme {
                 // A surface container using the 'background' color from the theme

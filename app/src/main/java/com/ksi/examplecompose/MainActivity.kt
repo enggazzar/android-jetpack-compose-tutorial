c64b71b54767decfdb21648f76a81c1b9a986b99
Mohamed Altohamy <mohamed.altohamy@kickstartinteractive.net>
2022-11-13 13:33:43 +0200

    ezz

--- app/src/main/java/com/ksi/examplecompose/MainActivity.kt
+++ app/src/main/java/com/ksi/examplecompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ksi.examplecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,25 +13,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ksi.examplecompose.ui.screen.SettingScreen
 import com.ksi.examplecompose.ui.theme.ExampleComposeTheme
 import com.ksi.examplecompose.ui.theme.defaultPadding
 import java.util.Collections.list
 
 class MainActivity : ComponentActivity() {
-    val viewModel :MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().also {
-             viewModel.isLoading
-        }
         setContent {
             ExampleComposeTheme {
                 // A surface container using the 'background' color from the theme
                 //hoistable state remove dublicate state
 
-                Surface(color = MaterialTheme.colors.background) {
-                    MyApp()
+                Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
+                   SettingScreen()
                 }
             }
         }
